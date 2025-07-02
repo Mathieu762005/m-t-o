74a2c70ce9f3ddacb829f6c5ed60604f1316b6be
Mathieu <mathieu762005@gmail.com>
2025-07-02 16:16:56 +0200

    maj

---
+++ script.js
@@ -0,0 +1,9 @@
+let city = 'havre'
+let apiKey = 'f85211be34b149d5fb745f44e506ac7b'
+let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&lang=fr&units=metric`
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
