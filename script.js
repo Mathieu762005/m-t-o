266048c6006e686486c404f8aeee6b6cb9fb1809
Mathieu <mathieu762005@gmail.com>
2025-07-03 11:55:53 +0200

    maj

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ fetch(url)
     .then(data => {
         console.log(data)
         console.log(data.city.name)
-        document.getElementById("city").innerHTML = `${data.city.name}`
-        document.getElementById("température").innerHTML = `${Math.round(data.list[0].main.temp)}°`
+        document.getElementById("heure").innerHTML = `${data.list[0].dt_txt}`
+        document.getElementById("city").innerHTML = `Le ${data.city.name}`
+        document.getElementById("température").innerHTML = `  ${Math.round(data.list[0].main.temp)}°`
         document.getElementById("description").innerHTML = `${data.list[0].weather[0].description}`
-        document.getElementById("tempMax").innerHTML = `${Math.round(data.list[0].main.temp_max)}°`
-        document.getElementById("tempMin").innerHTML = `${Math.round(data.list[0].main.temp_min)}°`
+        document.getElementById("tempMax").innerHTML = `<i class="bi bi-arrow-up"></i> ${Math.round(data.list[0].main.temp_max)}°`
+        document.getElementById("tempMin").innerHTML = `<i class="bi bi-arrow-down"></i> ${Math.round(data.list[0].main.temp_min)}°`
+        document.getElementById("soleil").innerHTML = `<img src="https://openweathermap.org/img/wn/01d@2x.png" alt="">`
+        document.getElementById("vent").innerHTML = `<i class="bi bi-wind"></i> ${data.list[0].wind.speed}`
+        document.getElementById("précipitation").innerHTML = `<i class="bi bi-tornado"></i> ${data.list[0].pop}%`
+        document.getElementById("humidity").innerHTML = `<i class="bi bi-droplet-half"></i> ${data.list[0].main.humidity}%`
     })
